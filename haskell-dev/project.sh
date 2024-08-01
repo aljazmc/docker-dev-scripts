c609d80a2219b23fb6e3f662536715a5929adb76
aljazmc <mlinaric.aljaz@gmail.com>
2024-08-01 07:46:39 +0200

    feat: add start function

--- haskell-dev/project.sh
+++ haskell-dev/project.sh
@@ -31,3 +31,40 @@ clean() {
         helloworld.o
 
 }
+
+start() {
+
+if [[ ! -f docker-compose.yml ]]; then
+    cat <<-EOF > docker-compose.yml
+services:
+    haskell:
+        image: haskell:latest
+        user: ${PROJECT_UID}:${PROJECT_GID}
+        working_dir: /home/$USER
+        environment:
+            DISPLAY: $DISPLAY
+            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+        volumes:
+            - .:/home/$USER
+            - /tmp/.X11-unix:/tmp/.X11-unix
+            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
+            - ~/.Xauthority:/root/.Xauthority
+        devices:
+            - /dev/dri:/dev/dri
+            - /dev/snd:/dev/snd
+        network_mode: host
+EOF
+fi
+
+if [[ ! -f helloworld.hs ]]; then
+    cat <<-EOF > helloworld.hs
+main :: IO ()
+main = putStrLn "Hello, World!"
+EOF
+fi
+
+    docker compose run --rm haskell sh -c "printenv"
+    docker compose run --rm haskell ghc helloworld.hs
+    docker compose run --rm haskell sh -c "./helloworld"
+
+}
