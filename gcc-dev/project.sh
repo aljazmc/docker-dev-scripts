4ad583529f7650817c7e533e106bca2db56c8712
aljazmc <mlinaric.aljaz@gmail.com>
2025-02-24 08:18:26 +0200

    style: change indentation

--- gcc-dev/project.sh
+++ gcc-dev/project.sh
@@ -3,13 +3,13 @@
 ## Check for linux and docker compose or quit
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
 if [[ ! -x "$(command -v compose version)" ]]; then
-    echo "Compose plugin is not installed. Exiting..."
-    exit
+  echo "Compose plugin is not installed. Exiting..."
+  exit
 fi
 
 ## Variables
@@ -22,50 +22,50 @@ PROJECT_GID=$(id -g)
 
 # docker-compose.yml
 if [ ! -f docker-compose.yml ]; then
-    cat << EOF > docker-compose.yml
+  cat << EOF > docker-compose.yml
 services:
-    gcc:
-        image: gcc:latest
-        user: $PROJECT_UID:$PROJECT_GID
-        working_dir: /usr/src/app
-        volumes:
-            - .:/usr/src/app
+  gcc:
+    image: gcc:latest
+    user: $PROJECT_UID:$PROJECT_GID
+    working_dir: /usr/src/app
+    volumes:
+      - .:/usr/src/app
 EOF
 fi
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        docker-compose.yml \
-        main \
-        main.c
+  docker compose down -v --rmi all --remove-orphans
+  rm -rf \
+    docker-compose.yml \
+    main \
+    main.c
 
 }
 
 gcc() {
 
 if [ ! -f main.c ]; then
-    cat<<EOF > main.c
+  cat<<EOF > main.c
 #include <stdio.h>
 
 int main(void)
 {
-    int i;
-    printf("Hello world!\n");
+  int i;
+  printf("Hello world!\n");
 }
 EOF
 fi
 
-    docker compose run --rm gcc sh -c "printenv"
-    docker compose run --rm gcc gcc main.c -o main
-    docker compose run --rm gcc sh -c "./main"
+  docker compose run --rm gcc sh -c "printenv"
+  docker compose run --rm gcc gcc main.c -o main
+  docker compose run --rm gcc sh -c "./main"
 
 }
 
 start() {
 
-    gcc
+  gcc
 
 }
 
