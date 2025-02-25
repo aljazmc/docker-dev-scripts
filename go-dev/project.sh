6af380c4edca75889950c709ece0099f0d2f63dd
aljazmc <mlinaric.aljaz@gmail.com>
2025-02-25 08:21:47 +0200

    style: change indentation

--- go-dev/project.sh
+++ go-dev/project.sh
@@ -3,13 +3,13 @@
 ## Check for linux and docker compose or quit
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
 if [[ ! -x "$(command -v compose version)" ]]; then
-    echo "Compose plugin is not installed. Exiting..."
-    exit
+  echo "Compose plugin is not installed. Exiting..."
+  exit
 fi
 
 ## Variables
@@ -24,53 +24,53 @@ PROJECT_GID=$(id -g)
 if [ ! -f docker-compose.yml ]; then
     cat << EOF > docker-compose.yml
 services:
-    golang:
-        image: golang:latest
-        user: $PROJECT_UID:$PROJECT_GID
-        working_dir: /usr/src/app
-        volumes:
-            - .:/usr/src/app
-            - .cache:/.cache
+  golang:
+    image: golang:latest
+    user: $PROJECT_UID:$PROJECT_GID
+    working_dir: /usr/src/app
+    volumes:
+      - .:/usr/src/app
+      - .cache:/.cache
 EOF
 fi
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        .cache \
-        docker-compose.yml \
-        main \
-        main.go
+  docker compose down -v --rmi all --remove-orphans
+  rm -rf \
+    .cache \
+    docker-compose.yml \
+    main \
+    main.go
 
 }
 
 golang() {
 
 if [ ! -d .cache ]; then
-    mkdir -p .cache
+  mkdir -p .cache
 fi
 if [ ! -f main.go ]; then
-    cat<<EOF > main.go
+  cat<<EOF > main.go
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!")
+  fmt.Println("Hello World!")
 }
 EOF
 fi
 
-    docker compose run --rm golang sh -c "printenv"
-    docker compose run --rm golang go build main.go
-    docker compose run --rm golang sh -c "./main"
+  docker compose run --rm golang sh -c "printenv"
+  docker compose run --rm golang go build main.go
+  docker compose run --rm golang sh -c "./main"
 
 }
 
 start() {
 
-    golang
+  golang
 
 }
 
