22eb8eef669e5581d7341291678885abed27f064
aljazmc <mlinaric.aljaz@gmail.com>
2025-02-26 08:03:25 +0200

    style: change indentation

--- heapsio-dev/project.sh
+++ heapsio-dev/project.sh
@@ -3,13 +3,13 @@
 ## Checks if OS is linux and docker compose is installed
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
 if [[ ! -x "$(command -v compose version)" ]]; then
-    echo "Compose plugin is not installed. Exiting..."
-    exit
+  echo "Compose plugin is not installed. Exiting..."
+  exit
 fi
 
 ## Variables
@@ -22,26 +22,26 @@ PROJECT_GID=$(id -g)
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        .cache \
-        .config \
-        compile.hxml \
-        docker-compose.yml \
-        Dockerfile \
-        hello.hl \
-        .haxelib \
-        haxelib \
-        src
+  docker compose down -v --rmi all --remove-orphans
+  rm -rf \
+    .cache \
+    .config \
+    compile.hxml \
+    docker-compose.yml \
+    Dockerfile \
+    hello.hl \
+    .haxelib \
+    haxelib \
+    src
 
 }
 
 start() {
 
-    mkdir -p src haxelib
+  mkdir -p src haxelib
 
 if [[ ! -f compile.hxml ]]; then
-    cat <<-EOF > compile.hxml
+  cat <<-EOF > compile.hxml
 -cp src
 -lib heaps
 -lib hlsdl
@@ -51,31 +51,31 @@ EOF
 fi
 
 if [[ ! -f Dockerfile ]]; then
-    cat <<EOF > Dockerfile
+  cat <<EOF > Dockerfile
 FROM haxe:latest
 
 ENV PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/haxelib:/usr/lib/haxe/lib"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-    g++ \
-    libglu1-mesa-dev \
-    libmbedtls-dev \
-    libopenal-dev \
-    libpng-dev \
-    libsdl2-dev \
-    libsqlite3-dev \
-    libturbojpeg-dev \
-    libuv1-dev \
-    libvorbis-dev \
-    make \
-    sudo && \
-    rm -rf /var/lib/apt/lists/* && \
-    mkdir -p /usr/src/hashlink /usr/lib/haxe/lib && \
-    cd /usr/src && \
-    git clone https://github.com/HaxeFoundation/hashlink && \
-    cd hashlink && \
-    make && \
-    make install
+  g++ \
+  libglu1-mesa-dev \
+  libmbedtls-dev \
+  libopenal-dev \
+  libpng-dev \
+  libsdl2-dev \
+  libsqlite3-dev \
+  libturbojpeg-dev \
+  libuv1-dev \
+  libvorbis-dev \
+  make \
+  sudo && \
+  rm -rf /var/lib/apt/lists/* && \
+  mkdir -p /usr/src/hashlink /usr/lib/haxe/lib && \
+  cd /usr/src && \
+  git clone https://github.com/HaxeFoundation/hashlink && \
+  cd hashlink && \
+  make && \
+  make install
 
 RUN groupadd -g $PROJECT_GID -r $USER
 RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
@@ -94,39 +94,39 @@ EOF
 fi
 
 if [[ ! -f docker-compose.yml ]]; then
-    cat <<-EOF > docker-compose.yml
+  cat <<-EOF > docker-compose.yml
 services:
-    heapsio-dev:
-        build: .
-        user: ${PROJECT_UID}:${PROJECT_GID}
-        working_dir: /home/$USER
-        environment:
-            DISPLAY: $DISPLAY
-            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
-        volumes:
-            - .:/home/$USER
-            - ./haxelib:/usr/lib/haxe/lib
-            - /tmp/.X11-unix:/tmp/.X11-unix
-            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
-            - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
-            - ~/.Xauthority:/root/.Xauthority
-        devices:
-            - /dev/dri:/dev/dri
-            - /dev/snd:/dev/snd
-        network_mode: host
+  heapsio-dev:
+    build: .
+    user: ${PROJECT_UID}:${PROJECT_GID}
+    working_dir: /home/$USER
+    environment:
+      DISPLAY: $DISPLAY
+      XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+    volumes:
+      - .:/home/$USER
+      - ./haxelib:/usr/lib/haxe/lib
+      - /tmp/.X11-unix:/tmp/.X11-unix
+      - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
+      - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
+      - ~/.Xauthority:/root/.Xauthority
+    devices:
+      - /dev/dri:/dev/dri
+      - /dev/snd:/dev/snd
+    network_mode: host
 EOF
 fi
 
 if [[ ! -f src/Main.hx ]]; then
-    cat <<-EOF > src/Main.hx
+  cat <<-EOF > src/Main.hx
 class Main extends hxd.App {
-    override function init() {
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello Hashlink !";
-    }
-    static function main() {
-        new Main();
-    }
+  override function init() {
+    var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+    tf.text = "Hello Hashlink !";
+  }
+  static function main() {
+    new Main();
+  }
 }
 EOF
 fi
