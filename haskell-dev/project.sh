f9570b893ffda4735732b1b7ac68a026353c6359
aljazmc <mlinaric.aljaz@gmail.com>
2025-02-28 10:33:48 +0200

    style: change indentation

--- haskell-dev/project.sh
+++ haskell-dev/project.sh
@@ -3,8 +3,8 @@
 ## Checks if OS is linux
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
 ## Variables
@@ -17,50 +17,50 @@ PROJECT_GID=$(id -g)
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        docker-compose.yml \
-        helloworld \
-        helloworld.hi \
-        helloworld.hs \
-        helloworld.o
+  docker compose down -v --rmi all --remove-orphans
+  rm -rf \
+    docker-compose.yml \
+    helloworld \
+    helloworld.hi \
+    helloworld.hs \
+    helloworld.o
 
 }
 
 start() {
 
 if [[ ! -f docker-compose.yml ]]; then
-    cat <<-EOF > docker-compose.yml
+  cat <<-EOF > docker-compose.yml
 services:
-    haskell:
-        image: haskell:latest
-        user: ${PROJECT_UID}:${PROJECT_GID}
-        working_dir: /home/$USER
-        environment:
-            DISPLAY: $DISPLAY
-            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
-        volumes:
-            - .:/home/$USER
-            - /tmp/.X11-unix:/tmp/.X11-unix
-            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
-            - ~/.Xauthority:/root/.Xauthority
-        devices:
-            - /dev/dri:/dev/dri
-            - /dev/snd:/dev/snd
-        network_mode: host
+  haskell:
+    image: haskell:latest
+    user: ${PROJECT_UID}:${PROJECT_GID}
+    working_dir: /home/$USER
+    environment:
+      DISPLAY: $DISPLAY
+      XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+    volumes:
+      - .:/home/$USER
+      - /tmp/.X11-unix:/tmp/.X11-unix
+      - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
+      - ~/.Xauthority:/root/.Xauthority
+    devices:
+      - /dev/dri:/dev/dri
+      - /dev/snd:/dev/snd
+    network_mode: host
 EOF
 fi
 
 if [[ ! -f helloworld.hs ]]; then
-    cat <<-EOF > helloworld.hs
+  cat <<-EOF > helloworld.hs
 main :: IO ()
 main = putStrLn "Hello, World!"
 EOF
 fi
 
-    docker compose run --rm haskell sh -c "printenv"
-    docker compose run --rm haskell ghc helloworld.hs
-    docker compose run --rm haskell sh -c "./helloworld"
+  docker compose run --rm haskell sh -c "printenv"
+  docker compose run --rm haskell ghc helloworld.hs
+  docker compose run --rm haskell sh -c "./helloworld"
 
 }
 
