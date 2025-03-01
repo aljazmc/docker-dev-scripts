06611c380b41a5d12359f2665920feafb9e93315
aljazmc <mlinaric.aljaz@gmail.com>
2025-03-01 11:31:17 +0200

    style: change indentation

--- java-dev/project.sh
+++ java-dev/project.sh
@@ -3,8 +3,8 @@
 ## Check for linux
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
 ## Variables
@@ -19,49 +19,49 @@ PROJECT_GID=$(id -g)
 
 # docker-compose.yml
 if [ ! -f docker-compose.yml ]; then
-    cat << EOF > docker-compose.yml
+  cat << EOF > docker-compose.yml
 services:
-    java-compiler:
-        image: eclipse-temurin:22-alpine
-        user: $PROJECT_UID:$PROJECT_GID
-        working_dir: /opt/app
-        volumes:
-            - .:/opt/app
+  java-compiler:
+    image: eclipse-temurin:22-alpine
+    user: $PROJECT_UID:$PROJECT_GID
+    working_dir: /opt/app
+    volumes:
+      - .:/opt/app
 EOF
 fi
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        docker-compose.yml \
-        HelloWorld.class \
-        HelloWorld.java
+  docker compose down -v --rmi all --remove-orphans
+  rm -rf \
+    docker-compose.yml \
+    HelloWorld.class \
+    HelloWorld.java
 
 }
 
 runjava() {
 
 if [ ! -f HelloWorld.java ]; then
-    cat<<EOF > HelloWorld.java
+  cat<<EOF > HelloWorld.java
 public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
-   }
+  public static void main(String[] args) {
+    // Prints "Hello, World" in the terminal window.
+    System.out.println("Hello, World");
+  }
 }
 EOF
 fi
 
-    docker compose run --rm java-compiler sh -c "printenv"
-    docker compose run --rm java-compiler javac HelloWorld.java
-    docker compose run --rm java-compiler java HelloWorld
+  docker compose run --rm java-compiler sh -c "printenv"
+  docker compose run --rm java-compiler javac HelloWorld.java
+  docker compose run --rm java-compiler java HelloWorld
 
 }
 
 start() {
 
-    runjava
+  runjava
 
 }
 
