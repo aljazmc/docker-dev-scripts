a9734c1b4afcdbd0a2a6d095839113cd4a68b99a
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-22 00:27:40 +0100

    refactor: haskell-dev project.sh

--- haskell-dev/project.sh
+++ haskell-dev/project.sh
@@ -10,74 +10,85 @@ PROJECT_GID=$(id -g)
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    docker-compose.yml \
-    helloworld \
-    helloworld.hi \
-    helloworld.hs \
-    helloworld.o
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf \
+        docker-compose.yml \
+        helloworld \
+        helloworld.hi \
+        helloworld.hs \
+        helloworld.o
 
 }
 
-start() {
+compose() {
 
-if [[ ! -f docker-compose.yml ]]; then
-  cat <<-EOF > docker-compose.yml
+if [ ! -f docker-compose.yml ]; then
+    cat <<-EOF > docker-compose.yml
 services:
-  haskell:
-    image: haskell:latest
-    working_dir: /home/$USER
-    volumes:
-      - .:/home/$USER
-    network_mode: host
+    haskell:
+        image: haskell:latest
+        working_dir: /home/$USER
+        volumes:
+            - .:/home/$USER
+        network_mode: host
 EOF
+fi
 
-################ GNU Linux variables block for docker-compose.yml #############
+}
 
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+composehack() {
+
 devices="$(cat <<-EOT
-   devices:\\
-      - /dev/dri:/dev/dri\\
-      - /dev/snd:/dev/snd
+       devices:\\
+            - /dev/dri:/dev/dri\\
+            - /dev/snd:/dev/snd
 EOT
 )"
 environment="$(cat <<-EOT
-   environment:\\
-      DISPLAY: $DISPLAY\\
-      XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+       environment:\\
+            DISPLAY: $DISPLAY\\
+            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
 EOT
 )"
 user="$(cat <<-EOT
-   user: $PROJECT_UID:$PROJECT_GID
+       user: $PROJECT_UID:$PROJECT_GID
 EOT
 )"
 volumes="$(cat <<-EOT
-     - /tmp/.X11-unix:/tmp/.X11-unix\\
-      - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}\\
-      - ~/.Xauthority:/root/.Xauthority
+           - /tmp/.X11-unix:/tmp/.X11-unix\\
+            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}\\
+            - ~/.Xauthority:/root/.Xauthority
 EOT
 )"
+
     echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
     sed -i "3a \ $user" docker-compose.yml
     sed -i "5a \ $environment"  docker-compose.yml
     sed -i "10a \ $volumes"  docker-compose.yml
     sed -i "13a \ $devices"  docker-compose.yml
-  fi
-fi
 
-###############################################################################
+}
 
-if [[ ! -f helloworld.hs ]]; then
-  cat <<-EOF > helloworld.hs
+start() {
+
+    compose
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+        composehack
+
+    fi
+
+    if [[ ! -f helloworld.hs ]]; then
+        cat <<-EOF > helloworld.hs
 main :: IO ()
 main = putStrLn "Hello, World!"
 EOF
-fi
+    fi
 
-  docker compose run --rm haskell sh -c "printenv"
-  docker compose run --rm haskell ghc helloworld.hs
-  docker compose run --rm haskell sh -c "./helloworld"
+    docker compose run --rm haskell ghc helloworld.hs
+    docker compose run --rm haskell sh -c "./helloworld"
+    docker compose run --rm haskell sh -c "printenv"
 
 }
 
