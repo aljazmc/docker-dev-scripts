af686d66338c58d82d9fffa102770ba2f8222729
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-21 11:47:33 +0100

    refactor: go-dev project.sh

--- go-dev/project.sh
+++ go-dev/project.sh
@@ -6,64 +6,81 @@
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
 
-## Configuration files
-
-# docker-compose.yml
-if [ ! -f docker-compose.yml ]; then
-    cat << EOF > docker-compose.yml
-services:
-  golang:
-    image: golang:latest
-    working_dir: /usr/src/app
-    volumes:
-      - .:/usr/src/app
-      - .cache:/.cache
-EOF
-
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
-  fi
-
-fi
+## Functions
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    .cache \
-    docker-compose.yml \
-    main \
-    main.go
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf \
+        .cache \
+        docker-compose.yml \
+        main \
+        main.go
+
+}
+
+compose() {
+
+if [ ! -f docker-compose.yml ]; then
+    cat << EOF > docker-compose.yml
+services:
+    golang:
+        image: golang:latest
+        working_dir: /usr/src/app
+        volumes:
+            - .:/usr/src/app
+            - .cache:/.cache
+EOF
+fi
+
+}
+
+composehack() {
+
+    if  ! grep -q "user" "docker-compose.yml"; then
+        echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
+        sed -i "/working_dir\:/{s@^\( \+\)@\1user\: $PROJECT_UID\:$PROJECT_GID\n\1@}" docker-compose.yml
+    fi
 
 }
 
 golang() {
 
 if [ ! -d .cache ]; then
-  mkdir -p .cache
+
+    mkdir -p .cache
+
 fi
+
 if [ ! -f main.go ]; then
-  cat<<EOF > main.go
+    cat<<EOF > main.go
 package main
 
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World!")
+    fmt.Println("Hello World!")
 }
 EOF
 fi
 
-  docker compose run --rm golang sh -c "printenv"
-  docker compose run --rm golang go build main.go
-  docker compose run --rm golang sh -c "./main"
+    docker compose run --rm golang go build main.go
+    docker compose run --rm golang sh -c "./main"
+    docker compose run --rm golang sh -c "printenv"
 
 }
 
 start() {
 
-  golang
+    compose
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+        composehack
+
+    fi
+
+    golang
 
 }
 
