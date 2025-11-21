bea3e9a9dbfce481d106978379d2128ea31c8883
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-21 12:48:10 +0100

    refactor: rust-dev project.sh

--- rust-dev/project.sh
+++ rust-dev/project.sh
@@ -6,55 +6,70 @@
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
 
-## Configuration files
-
-# docker-compose.yml
-if [ ! -f docker-compose.yml ]; then
-  cat << EOF > docker-compose.yml
-services:
-  rust:
-    image: rust:latest
-    working_dir: /usr/src/app
-    volumes:
-      - .:/usr/src/app
-EOF
-
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
-  fi
-
-fi
+## Functions
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    docker-compose.yml \
-    hello \
-    hello.rs
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf \
+        docker-compose.yml \
+        hello \
+        hello.rs
+
+}
+
+compose() {
+
+if [ ! -f docker-compose.yml ]; then
+    cat << EOF > docker-compose.yml
+services:
+    rust:
+        image: rust:latest
+        working_dir: /usr/src/app
+        volumes:
+            - .:/usr/src/app
+EOF
+fi
+
+}
+
+composehack() {
+
+    if  ! grep -q "user" "docker-compose.yml"; then
+        echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
+        sed -i "/working_dir\:/{s@^\( \+\)@\1user\: $PROJECT_UID\:$PROJECT_GID\n\1@}" docker-compose.yml
+    fi
 
 }
 
 rust() {
 
 if [ ! -f hello.rs ]; then
-  cat<<EOF > hello.rs
+    cat<<EOF > hello.rs
 fn main() {
-  println!("Hello World!");
+    println!("Hello World!");
 }
 EOF
 fi
 
-  docker compose run --rm rust sh -c "printenv"
-  docker compose run --rm rust rustc hello.rs
-  docker compose run --rm rust sh -c "./hello"
+    docker compose run --rm rust rustc hello.rs
+    docker compose run --rm rust sh -c "./hello"
+    docker compose run --rm rust sh -c "printenv"
 
 }
 
 start() {
 
-  rust
+    compose
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+        composehack
+
+    fi
+
+    rust
 
 }
 
+"$1"
