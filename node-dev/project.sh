adf015b2455c4295b6f2d9aa4fd40927e0c5a41d
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-21 09:08:59 +0100

    refactor: node-dev project.sh

--- node-dev/project.sh
+++ node-dev/project.sh
@@ -6,64 +6,87 @@
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
 
-## Configuration files
-
-# docker-compose.yml
-if [ ! -f docker-compose.yml ]; then
-  cat << EOF > docker-compose.yml
-services:
-  node:
-    image: node:current-alpine
-    working_dir: "$PWD"
-    volumes:
-      - .:$PWD
-    environment:
-      HOME:       "$PWD"
-      NODE_ENV:   development
-      PATH:       "$PWD/.yarn/bin:$PWD/node_modules/.bin:\$PATH"
-    network_mode: host
-EOF
-
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
-  fi
-
-fi
+## Functions
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    docker-compose.yml \
-    node_modules \
-    package.json \
-    yarn.lock \
-    .cache \
-    .npm \
-    .yarn/berry \
-    .yarn/bin \
-    .yarn/unplugged \
-    .yarn/install-state.gz \
-    .yarnrc
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf \
+        dist \
+        docker-compose.yml \
+        node_modules \
+        package.json \
+        yarn.lock \
+        .cache \
+        .pnp.cjs \
+        .pnp.loader.mjs \
+        .vim \
+        .vimrc \
+        .yarn/berry \
+        .yarn/bin \
+        .yarn/sdks \
+        .yarn/unplugged \
+        .yarn/install-state.gz \
+        .yarnrc
+
+}
+
+
+compose() {
+
+if [ ! -f docker-compose.yml ]; then
+    cat << EOF > docker-compose.yml
+services:
+    node:
+        image: node:current-alpine
+        working_dir: "$PWD"
+        volumes:
+            - .:$PWD
+        environment:
+            HOME:               "$PWD"
+            NODE_ENV:           development
+        network_mode: host
+EOF
+fi
+
+}
+
+composehack() {
+
+    if  ! grep -q "user" "docker-compose.yml"; then
+        echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
+        sed -i "/working_dir\:/{s@^\( \+\)@\1user\: $PROJECT_UID\:$PROJECT_GID\n\1@}" docker-compose.yml
+    fi
 
 }
 
 node() {
 
 if [ ! -f package.json ]; then
-  docker compose run --rm node yarn init
+
+    docker compose run --rm node yarn init
+
 else
-  docker compose run --rm node yarn install
+
+    docker compose run --rm node yarn install
+
 fi
 
-  docker compose run --rm node sh -c "printenv"
+docker compose run --rm node sh -c "printenv"
 
 }
 
 start() {
 
-  node
+    compose
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+        composehack
+
+    fi
+
+    node
 
 }
 
