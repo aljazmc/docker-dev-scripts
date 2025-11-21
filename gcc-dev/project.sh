dd3b337e411ff97ea2745529b52cc59a592e1262
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-21 11:18:42 +0100

    refactor: gcc-dev project.sh

--- gcc-dev/project.sh
+++ gcc-dev/project.sh
@@ -6,59 +6,73 @@
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
 
-## Configuration files
-
-# docker-compose.yml
-if [ ! -f docker-compose.yml ]; then
-  cat << EOF > docker-compose.yml
-services:
-  gcc:
-    image: gcc:latest
-    working_dir: /usr/src/app
-    volumes:
-      - .:/usr/src/app
-EOF
-
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
-  fi
-
-fi
+## Functions
 
 clean() {
 
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    docker-compose.yml \
-    main \
-    main.c
+    docker compose down -v --rmi all --remove-orphans
+    rm -rf \
+        docker-compose.yml \
+        main \
+        main.c
+
+}
+
+compose() {
+
+if [ ! -f docker-compose.yml ]; then
+    cat << EOF > docker-compose.yml
+services:
+    gcc:
+        image: gcc:latest
+        working_dir: /usr/src/app
+        volumes:
+            - .:/usr/src/app
+EOF
+fi
+
+}
+
+composehack() {
+
+    if  ! grep -q "user" "docker-compose.yml"; then
+        echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
+        sed -i "/working_dir\:/{s@^\( \+\)@\1user\: $PROJECT_UID\:$PROJECT_GID\n\1@}" docker-compose.yml
+    fi
 
 }
 
 gcc() {
 
 if [ ! -f main.c ]; then
-  cat<<EOF > main.c
+    cat<<EOF > main.c
 #include <stdio.h>
 
 int main(void)
 {
-  int i;
-  printf("Hello world!\n");
+    int i;
+    printf("Hello world!\n");
 }
 EOF
 fi
 
-  docker compose run --rm gcc sh -c "printenv"
-  docker compose run --rm gcc gcc main.c -o main
-  docker compose run --rm gcc sh -c "./main"
+    docker compose run --rm gcc gcc main.c -o main
+    docker compose run --rm gcc sh -c "./main"
+    docker compose run --rm gcc sh -c "printenv"
 
 }
 
 start() {
 
-  gcc
+    compose
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+        composehack
+
+    fi
+
+    gcc
 
 }
 
