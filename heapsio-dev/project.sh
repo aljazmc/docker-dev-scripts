ba2e38e5bd3c689e22a5663532d6d97b5e06d4b5
aljazmc <mlinaric.aljaz@gmail.com>
2025-11-23 00:47:20 +0100

    refactor: heapsio-dev project.sh

--- heapsio-dev/project.sh
+++ heapsio-dev/project.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "This script works only on GNU/Linux systems"
+    exit
+fi
+
 ## Variables
 
 #PROJECT_NAME=`echo ${PWD##*/}` ## PROJECT_NAME = parent directory
@@ -9,27 +14,28 @@ PROJECT_GID=$(id -g)
 ## Functions
 
 clean() {
-
-  docker compose down -v --rmi all --remove-orphans
-  rm -rf \
-    .cache \
-    .config \
-    compile.hxml \
-    docker-compose.yml \
-    Dockerfile \
-    hello.hl \
-    .haxelib \
-    haxelib \
-    src
+	
+    docker compose down -v --rmi all --remove-orphans
+    docker system prune -af --volumes
+    rm -rf \
+        .cache \
+        .config \
+        compile.hxml \
+        docker-compose.yml \
+        Dockerfile \
+        hello.hl \
+        .haxelib \
+        haxelib \
+        src
 
 }
 
 start() {
 
-  mkdir -p src haxelib
+    mkdir -p src haxelib
 
 if [[ ! -f compile.hxml ]]; then
-  cat <<-EOF > compile.hxml
+    cat <<-EOF > compile.hxml
 -cp src
 -lib format
 -lib heaps
@@ -40,32 +46,32 @@ EOF
 fi
 
 if [[ ! -f Dockerfile ]]; then
-  cat <<EOF > Dockerfile
+    cat <<EOF > Dockerfile
 FROM haxe:latest
 
 ENV PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/haxelib:/usr/lib/haxe/lib"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-  g++ \
-  libglu1-mesa-dev \
-  libmbedtls-dev \
-  libopenal-dev \
-  libpng-dev \
-  libsdl2-dev \
-  libsqlite3-dev \
-  libturbojpeg0-dev \
-  libuv1-dev \
-  libvorbis-dev \
-  make \
-  sudo && \
-  rm -rf /var/lib/apt/lists/* && \
-  mkdir -p /usr/src/hashlink /usr/lib/haxe/lib && \
-  cd /usr/src && \
-  git clone https://github.com/HaxeFoundation/hashlink && \
-  cd hashlink && \
-  git checkout 82ea54ca && \
-  make && \
-  make install
+    g++ \
+    libglu1-mesa-dev \
+    libmbedtls-dev \
+    libopenal-dev \
+    libpng-dev \
+    libsdl2-dev \
+    libsqlite3-dev \
+    libturbojpeg0-dev \
+    libuv1-dev \
+    libvorbis-dev \
+    make \
+    sudo && \
+    rm -rf /var/lib/apt/lists/* && \
+    mkdir -p /usr/src/hashlink /usr/lib/haxe/lib && \
+    cd /usr/src && \
+    git clone https://github.com/HaxeFoundation/hashlink && \
+    cd hashlink && \
+    git checkout 82ea54ca && \
+    make && \
+    make install
 
 RUN groupadd -g $PROJECT_GID -r $USER
 RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
@@ -84,52 +90,56 @@ EOF
 fi
 
 if [[ ! -f docker-compose.yml ]]; then
-  cat <<-EOF > docker-compose.yml
+    cat <<-EOF > docker-compose.yml
 services:
-  heapsio-dev:
-    build: .
-    working_dir: /home/$USER
-    user: $PROJECT_UID:$PROJECT_GID
-    environment:
-      DISPLAY: $DISPLAY
-      XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
-    volumes:
-      - .:/home/$USER
-      - ./haxelib:/usr/lib/haxe/lib
-      - /tmp/.X11-unix:/tmp/.X11-unix
-      - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
-      - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
-      - ~/.Xauthority:/root/.Xauthority
-    devices:
-      - /dev/dri:/dev/dri
-      - /dev/snd:/dev/snd
-    network_mode: host
+    heapsio-dev:
+        build: .
+        working_dir: /home/$USER
+        user: $PROJECT_UID:$PROJECT_GID
+        environment:
+            DISPLAY: $DISPLAY
+            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+        volumes:
+            - .:/home/$USER
+            - ./haxelib:/usr/lib/haxe/lib
+            - /tmp/.X11-unix:/tmp/.X11-unix
+            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
+            - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
+            - ~/.Xauthority:/root/.Xauthority
+        devices:
+            - /dev/dri:/dev/dri
+            - /dev/snd:/dev/snd
+        network_mode: host
 EOF
 fi
 
 if [[ ! -f src/Main.hx ]]; then
-  cat <<-EOF > src/Main.hx
+    cat <<-EOF > src/Main.hx
 class Main extends hxd.App {
-  override function init() {
-    var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-    tf.text = "Hello Hashlink !";
-  }
-  static function main() {
-    new Main();
-  }
+    override function init() {
+        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+        tf.text = "Hello Hashlink !";
+    }
+    static function main() {
+        new Main();
+    }
 }
 EOF
 fi
 
-[ ! -d haxelib/heaps ]      && docker compose run --rm heapsio-dev bash -c "haxelib setup && haxelib install format"
+docker compose run --rm heapsio-dev bash -c "haxelib setup \
+    && haxelib --global update haxelib \
+    && haxelib fixrepo"
+
+[ ! -d haxelib/format ]     && docker compose run --rm heapsio-dev bash -c "haxelib install format"
 [ ! -d haxelib/heaps ]      && docker compose run --rm heapsio-dev bash -c "haxelib install heaps"
 [ ! -d haxelib/hlopenal ]   && docker compose run --rm heapsio-dev bash -c "haxelib install hlopenal"
 [ ! -d haxelib/hlsdl ]      && docker compose run --rm heapsio-dev bash -c "haxelib install hlsdl"
 [ ! -d haxelib/hldx ]       && docker compose run --rm heapsio-dev bash -c "haxelib install hldx"
 
-  docker compose run --rm heapsio-dev sh -c "printenv"
-  docker compose run --rm heapsio-dev haxe compile.hxml
-  docker compose run --rm heapsio-dev hl hello.hl
+    docker compose run --rm heapsio-dev haxe compile.hxml
+    docker compose run --rm heapsio-dev hl hello.hl
+    docker compose run --rm heapsio-dev sh -c "printenv"
 
 }
 
