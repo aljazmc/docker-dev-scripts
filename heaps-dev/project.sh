90a11e6fe9b692f7f7dc9eb9e1925d8cfed15140
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-04 23:51:03 +0200

    refactor: separate Dockerfile

--- heaps-dev/project.sh
+++ heaps-dev/project.sh
@@ -10,23 +10,21 @@ fi
 #PROJECT_NAME=`echo ${PWD##*/}` ## PROJECT_NAME = parent directory
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
+USER=heaps
 
 ## Functions
 
 clean() {
-	
-    docker compose down -v --rmi all --remove-orphans
-    docker system prune -af --volumes
-    rm -rf \
-        .cache \
-        .config \
-        compile.hxml \
-        docker-compose.yml \
-        Dockerfile \
-        hello.hl \
-        .haxelib \
-        haxelib \
-        src
+
+docker compose down -v --rmi all --remove-orphans
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+    | sed "
+        /Dockerfile/d;
+        /project.sh/d;
+    " \
+    | xargs -I {} rm -rf {}
 
 }
 
@@ -45,54 +43,10 @@ if [[ ! -f compile.hxml ]]; then
 EOF
 fi
 
-if [[ ! -f Dockerfile ]]; then
-    cat <<EOF > Dockerfile
-FROM haxe:latest
-
-ENV PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/haxelib:/usr/lib/haxe/lib"
-
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    g++ \
-    libglu1-mesa-dev \
-    libmbedtls-dev \
-    libopenal-dev \
-    libpng-dev \
-    libsdl2-dev \
-    libsqlite3-dev \
-    libturbojpeg0-dev \
-    libuv1-dev \
-    libvorbis-dev \
-    make \
-    sudo && \
-    rm -rf /var/lib/apt/lists/* && \
-    mkdir -p /usr/src/hashlink /usr/lib/haxe/lib && \
-    cd /usr/src && \
-    git clone https://github.com/HaxeFoundation/hashlink && \
-    cd hashlink && \
-    git checkout latest && \
-    make && \
-    make install
-
-RUN groupadd -g $PROJECT_GID -r $USER
-RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
-
-#Change password
-RUN echo "$USER:$USER" | chpasswd
-#Make sudo passwordless
-RUN echo "${USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-$USER
-RUN usermod -aG sudo $USER
-
-USER $USER
-WORKDIR /home/$USER
-
-CMD ["/bin/bash"]
-EOF
-fi
-
 if [[ ! -f docker-compose.yml ]]; then
     cat <<-EOF > docker-compose.yml
 services:
-    heapsio-dev:
+    heaps:
         build: .
         working_dir: /home/$USER
         user: $PROJECT_UID:$PROJECT_GID
@@ -101,11 +55,10 @@ services:
             XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
         volumes:
             - .:/home/$USER
-            - ./haxelib:/usr/lib/haxe/lib
             - /tmp/.X11-unix:/tmp/.X11-unix
             - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}
             - /var/lib/dbus/machine-id:/var/lib/dbus/machine-id
-            - ~/.Xauthority:/root/.Xauthority
+            - ~/.Xauthority:/home/$USER/.Xauthority
         devices:
             - /dev/dri:/dev/dri
             - /dev/snd:/dev/snd
@@ -127,19 +80,20 @@ class Main extends hxd.App {
 EOF
 fi
 
-docker compose run --rm heapsio-dev bash -c "haxelib setup \
+docker compose run --rm heaps bash -c " \
+    haxelib setup haxelib \
     && haxelib --global update haxelib \
     && haxelib fixrepo"
 
-[ ! -d haxelib/format ]     && docker compose run --rm heapsio-dev bash -c "haxelib install format"
-[ ! -d haxelib/heaps ]      && docker compose run --rm heapsio-dev bash -c "haxelib install heaps"
-[ ! -d haxelib/hlopenal ]   && docker compose run --rm heapsio-dev bash -c "haxelib install hlopenal"
-[ ! -d haxelib/hlsdl ]      && docker compose run --rm heapsio-dev bash -c "haxelib install hlsdl"
-[ ! -d haxelib/hldx ]       && docker compose run --rm heapsio-dev bash -c "haxelib install hldx"
+[ ! -d haxelib/format 	]   && docker compose run --rm heaps bash -c "haxelib install format"
+[ ! -d haxelib/heaps 	]   && docker compose run --rm heaps bash -c "haxelib install heaps"
+[ ! -d haxelib/hlopenal ]   && docker compose run --rm heaps bash -c "haxelib install hlopenal"
+[ ! -d haxelib/hlsdl 	]   && docker compose run --rm heaps bash -c "haxelib install hlsdl"
+[ ! -d haxelib/hldx 	]   && docker compose run --rm heaps bash -c "haxelib install hldx"
 
-    docker compose run --rm heapsio-dev haxe compile.hxml
-    docker compose run --rm heapsio-dev hl hello.hl
-    docker compose run --rm heapsio-dev sh -c "printenv"
+    docker compose run --rm heaps haxe compile.hxml
+    docker compose run --rm heaps hl hello.hl
+    docker compose run --rm heaps sh -c "printenv"
 
 }
 
