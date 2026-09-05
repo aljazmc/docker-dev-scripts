3fc62ef51468e23f24b235327578bb62fe3e416e
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-05 14:07:35 +0200

    refactor: separate Dockerfile

--- ocaml-dev/project.sh
+++ ocaml-dev/project.sh
@@ -19,18 +19,21 @@ fi
 #PROJECT_NAME=`echo ${PWD##*/}` ## PROJECT_NAME = parent directory
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
+USER=ocaml
 
 ## Functions
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        docker-compose.yml \
-        Dockerfile \
-        hello.ml \
-        helloworld \
-        .opam
+docker compose down -v --rmi all --remove-orphans
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+    | sed "
+        /Dockerfile/d;
+        /project.sh/d;
+    " \
+    | xargs -I {} rm -rf {}
 
 }
 
@@ -39,11 +42,10 @@ start() {
 if [[ ! -f docker-compose.yml ]]; then
     cat <<-EOF > docker-compose.yml
 services:
-    ocamlopam:
-        image: ocaml/opam:debian
+    ocaml:
+        build: .
         user: $PROJECT_UID:$PROJECT_GID
         working_dir: /home/$USER
-        command: /bin/sh -c "opam init --root=/home/$USER/.opam"
         environment:
             DISPLAY: $DISPLAY
             XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
@@ -59,31 +61,21 @@ services:
 EOF
 fi
 
-if [[ ! -d .opam ]]; then
-opamroot="$(cat <<-EOT
-           OPAMROOT: /home/$USER/.opam
-EOT
-)"
+if [ ! -d .opam ]; then
 
-    docker compose run --rm ocamlopam
-  
-    if grep "OPAMROOT" docker-compose.yml
-    then
-        echo "\$OPAMROOT is already present in docker-compose.yml"
-    else
-        echo "adding \$OPAMROOT to docker-compose.yml"
-        sed -i "7a \ $opamroot" docker-compose.yml
-    fi
-  
-    docker compose run --rm ocamlopam opam config list
-    docker compose run --rm ocamlopam opam install core
-    docker compose run --rm ocamlopam dune init proj helloworld
+   docker compose run --rm ocaml sh -c " \
+        yes | opam init --disable-sandboxing \
+        && yes | opam config list \
+        && yes | opam install core \
+	&& echo "test -r '/home/ocaml/.opam/opam-init/init.sh' && . '/home/ocaml/.opam/opam-init/init.sh' > /dev/null 2> /dev/null || true" >> ~/.profile \
+        && dune init proj helloworld"
 
 fi
 
-    docker compose run --rm ocamlopam sh -c "cd helloworld && dune build"
-    docker compose run --rm ocamlopam sh -c "cd helloworld && dune exec helloworld"
-    docker compose run --rm ocamlopam sh -c "printenv"
+    docker compose run --rm ocaml sh -c "cd helloworld \
+        && dune build \
+        && dune exec helloworld \
+        && printenv"
 
 }
 
