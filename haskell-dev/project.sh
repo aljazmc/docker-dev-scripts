64b8b4ec06e821791038362a6407b13164ba5b7b
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-05 11:19:53 +0200

    refactor: separate Dockerfile

--- haskell-dev/project.sh
+++ haskell-dev/project.sh
@@ -5,18 +5,21 @@
 #PROJECT_NAME=`echo ${PWD##*/}` ## PROJECT_NAME = parent directory
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
+USER=haskell
 
 ## Functions
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    rm -rf \
-        docker-compose.yml \
-        helloworld \
-        helloworld.hi \
-        helloworld.hs \
-        helloworld.o
+docker compose down -v --rmi all --remove-orphans
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+    | sed "
+        /Dockerfile/d;
+        /project.sh/d;
+    " \
+    | xargs -I {} rm -rf {}
 
 }
 
@@ -26,58 +29,19 @@ if [[ ! -f docker-compose.yml ]]; then
     cat <<-EOF > docker-compose.yml
 services:
     haskell:
-        image: haskell:latest
+        build: .
+        user: ${PROJECT_UID}:${PROJECT_GID}
         working_dir: /home/$USER
         volumes:
             - .:/home/$USER
-        network_mode: host
 EOF
 fi
-
-}
-
-composehack() {
-
-devices="$(cat <<-EOT
-       devices:\\
-            - /dev/dri:/dev/dri\\
-            - /dev/snd:/dev/snd
-EOT
-)"
-environment="$(cat <<-EOT
-       environment:\\
-            DISPLAY: $DISPLAY\\
-            XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
-EOT
-)"
-user="$(cat <<-EOT
-       user: $PROJECT_UID:$PROJECT_GID
-EOT
-)"
-volumes="$(cat <<-EOT
-           - /tmp/.X11-unix:/tmp/.X11-unix\\
-            - /run/user/${PROJECT_UID}:/run/user/${PROJECT_UID}\\
-            - ~/.Xauthority:/root/.Xauthority
-EOT
-)"
-
-    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3a \ $user" docker-compose.yml
-    sed -i "5a \ $environment"  docker-compose.yml
-    sed -i "10a \ $volumes"  docker-compose.yml
-    sed -i "13a \ $devices"  docker-compose.yml
 
 }
 
 start() {
 
     compose
-
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-
-        composehack
-
-    fi
 
     if [[ ! -f helloworld.hs ]]; then
         cat <<-EOF > helloworld.hs
@@ -86,9 +50,10 @@ main = putStrLn "Hello, World!"
 EOF
     fi
 
-    docker compose run --rm haskell ghc helloworld.hs
-    docker compose run --rm haskell sh -c "./helloworld"
-    docker compose run --rm haskell sh -c "printenv"
+    docker compose run --rm haskell sh -c " \
+        stack ghc helloworld.hs \
+        && ./helloworld \
+	&& printenv"
 
 }
 
