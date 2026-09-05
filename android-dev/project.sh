9aeae96dbb33920c2b073ad1f8eb858080e1d97d
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-05 17:55:42 +0200

    refactor: separate Dockerfile

--- android-dev/project.sh
+++ android-dev/project.sh
@@ -7,34 +7,26 @@ KVMOWNER=$(ls -l /dev/kvm | awk '{print $3}')
 PROJECT_UID=$(id -u)
 PROJECT_GID=$(id -g)
 SDK=$(echo /home/"$USER"/sdk)
+USER=android
 
-if [[ "$KVMOWNER" != "$USER" ]]; then 
-    echo "'/dev/kvm' is not owned by the current user. Aborting..."
-    exit
-fi
+# if [[ "$KVMOWNER" != "$(id -u):$(id -g)" ]]; then 
+#     echo "'/dev/kvm' is not owned by the current user. Aborting..."
+#     exit
+# fi
 
 ## Functions
 
 clean() {
 
-    docker compose down -v --rmi all --remove-orphans
-    docker system prune -af --volumes
-    rm -rf \
-        .android \
-        app \
-        .cache \
-        .config \
-        docker-compose.yml \
-        Dockerfile \
-        .emulator_console_auth_token \
-        .gradle \
-        .knownPackages \
-        .kotlin \
-        .pki \
-        sdk \
-        .temp \
-        .wget-hsts \
-        ./*.zip
+docker compose down -v --rmi all --remove-orphans
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+    | sed "
+        /Dockerfile/d;
+        /project.sh/d;
+    " \
+    | xargs -I {} rm -rf {}
 
 }
 
@@ -182,82 +174,15 @@ start() {
 
     mkdir -p app sdk/cmdline-tools
 
-if [[ ! -f Dockerfile ]]; then
-    cat <<EOF > Dockerfile
-FROM debian:latest
-
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    android-udev-rules \
-    ca-certificates \
-    default-jdk \
-    libpulse-dev \
-    libxcb-cursor-dev \
-    libxkbfile-dev \
-    libfontconfig1-dev \
-    libfreetype-dev \
-    libgtk-3-dev \
-    libx11-dev \
-    libx11-xcb-dev \
-    libxcb-cursor-dev \
-    libxcb-glx0-dev \
-    libxcb-icccm4-dev \
-    libxcb-image0-dev \
-    libxcb-keysyms1-dev \
-    libxcb-randr0-dev \
-    libxcb-render-util0-dev \
-    libxcb-shape0-dev \
-    libxcb-shm0-dev \
-    libxcb-sync-dev \
-    libxcb-util-dev \
-    libxcb-xfixes0-dev \
-    libxcb-xkb-dev \
-    libxcb1-dev \
-    libxext-dev \
-    libxfixes-dev \
-    libxi-dev \
-    libxkbcommon-dev \
-    libxkbcommon-x11-dev \
-    libxrender-dev \
-    qemu-system-x86 \
-    sudo \
-    udev \
-    unzip \
-    wget \
-    zip \
-    && rm -rf /var/lib/apt/lists/*
-
-RUN groupadd -g $PROJECT_GID -r $USER
-RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
-
-#Change password
-RUN echo "$USER:$USER" | chpasswd
-#Make sudo passwordless
-RUN echo "${USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-$USER
-RUN usermod -aG sudo $USER
-RUN usermod -aG kvm $USER
-
-USER $USER
-WORKDIR /home/$USER
-
-CMD ["/bin/bash"]
-EOF
-fi
-
 if [[ ! -f docker-compose.yml ]]; then
     cat <<-EOF > docker-compose.yml
 services:
-    androidsdk:
+    android:
         build: .
         working_dir: /home/$USER
         user: $PROJECT_UID:$PROJECT_GID
         environment:
-            ANDROID_HOME: $SDK
-            ANDROID_USER_HOME: /home/$USER/.android
-            ANDROID_SDK_ROOT: $SDK
             DISPLAY: $DISPLAY
-            GRADLE_HOME: $SDK/gradle-9.5.1/bin
-            GRADLE_USER_HOME: /home/$USER/.gradle
-            PATH: "/home/$USER/app:$SDK/build-tools/35.0.0:$SDK/gradle-9.5.1/bin:$SDK/kotlinc/bin:$SDK/emulator:$SDK/cmdline-tools/latest/bin:$SDK/platform-tools:\$PATH"
             XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
         volumes:
             - .:/home/$USER
@@ -273,41 +198,42 @@ services:
 EOF
 fi
 
-    docker compose run --rm androidsdk sh -c "wget https://dl.google.com/android/repository/commandlinetools-linux-14742923_latest.zip && \
-        unzip commandlinetools-linux-*_latest.zip cmdline-tools/* -d sdk/cmdline-tools && \
-        cd sdk/cmdline-tools && \
-        mv cmdline-tools latest && \
-        rm ../../commandlinetools-linux-*_latest.zip"
-    docker compose run --rm androidsdk sh -c "wget https://services.gradle.org/distributions/gradle-9.5.1-bin.zip && \
-        unzip gradle-9.5.1-bin.zip -d sdk && \
-        rm gradle-*-bin.zip"
-    docker compose run --rm androidsdk sh -c "wget https://github.com/JetBrains/kotlin/releases/download/v2.4.0/kotlin-compiler-2.4.0.zip && \
-        unzip kotlin-compiler-2.4.0.zip -d sdk && \
-        rm kotlin-compiler-*.zip"
-    docker compose run --rm androidsdk sh -c "yes | sdkmanager --licenses"
-    docker compose run --rm androidsdk sh -c "sdkmanager --update && \
-        sdkmanager \
+    docker compose run --rm android sh -c " \
+        wget https://dl.google.com/android/repository/commandlinetools-linux-14742923_latest.zip \
+        && unzip commandlinetools-linux-*_latest.zip cmdline-tools/* -d sdk/cmdline-tools \
+        && cd sdk/cmdline-tools \
+        && mv cmdline-tools latest \
+        && rm ../../commandlinetools-linux-*_latest.zip \
+	&& cd /home/android \
+        && wget https://services.gradle.org/distributions/gradle-9.5.1-bin.zip \
+        && unzip gradle-9.5.1-bin.zip -d sdk \
+        && rm gradle-*-bin.zip \
+        && wget https://github.com/JetBrains/kotlin/releases/download/v2.4.0/kotlin-compiler-2.4.0.zip \
+        && unzip kotlin-compiler-2.4.0.zip -d sdk \
+        && rm kotlin-compiler-*.zip \
+        && yes | sdkmanager --licenses \
+        && sdkmanager --update \
+        && sdkmanager \
             'build-tools;35.0.0' \
             'cmake;4.1.2' \
             'emulator' \
             'ndk;30.0.14904198' \
             'platform-tools' \
             'platforms;android-36' \
-            'system-images;android-36;default;x86_64' "
-    docker compose run --rm androidsdk sh -c "sdkmanager --list"
-    docker compose run --rm androidsdk sh -c "printenv"
-    
-    docker compose run --rm androidsdk sh -c "cd app && yes | gradle init --type kotlin-application --dsl kotlin"
-    docker compose run --rm androidsdk sh -c "echo 'no' | avdmanager create avd -n 1 -k 'system-images;android-36;default;x86_64'"
+            'system-images;android-36;default;x86_64' \
+	&& sdkmanager --list \
+        && cd app \
+	&& yes | gradle init --type kotlin-application --dsl kotlin \
+        && echo 'no' | avdmanager create avd -n 1 -k 'system-images;android-36;default;x86_64'"
   
-    mate-terminal -- sh -c "docker compose run --rm androidsdk sh -c 'emulator -avd 1'"
+    mate-terminal -- sh -c "docker compose run --rm android sh -c 'emulator -avd 1'"
   
     confighack
     
-    docker compose run --rm androidsdk sh -c "cd app && gradlew tasks"
-    docker compose run --rm androidsdk sh -c "cd app && gradlew build"
-    docker compose run --rm androidsdk sh -c "cd app && gradlew installDebug"
-    docker compose run --rm androidsdk sh -c "adb shell 'am start -n org.example/org.example.App'"
+    docker compose run --rm android sh -c "cd app && gradlew tasks"
+    docker compose run --rm android sh -c "cd app && gradlew build"
+    docker compose run --rm android sh -c "cd app && gradlew installDebug"
+    docker compose run --rm android sh -c "adb shell 'am start -n org.example/org.example.App'"
   
 }
 
